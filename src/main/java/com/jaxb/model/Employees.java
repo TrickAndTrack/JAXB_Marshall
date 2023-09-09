475bd52798b809ec70b7b9c1c5cdc96eacc54dd3
TrickAndTrack <nahushzilpilwar@gmail.com>
2023-09-10 00:18:56 +0530

    New Employees Model added

---
+++ src/main/java/com/jaxb/model/Employees.java
@@ -0,0 +1,26 @@
+package com.jaxb.model;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
+@XmlRootElement(name = "employees")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Employees implements Serializable
+{
+    @XmlElement(name = "employee_rec", required = true)
+    private List<Employee> employees ;
+
+
+}
